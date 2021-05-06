8b7ed097895e1c5e2d046c01a4a4e3ce2da133eb
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2021-05-07 03:28:35 +0400

    Adding startup to gateway-translator

---
+++ src/gateway-translator/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(gateway_translator.Startup))]
+namespace gateway_translator
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            
+        }
+    }
+}
