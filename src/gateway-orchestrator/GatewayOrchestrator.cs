973096454c65c1103f7e6615c3a297991d367c40
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2021-05-09 22:36:03 +0400

    Adding device id in orchestrator

--- src/gateway-orchestrator/GatewayOrchestrator.cs
+++ src/gateway-orchestrator/GatewayOrchestrator.cs
@@ -12,31 +12,37 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
-namespace gateway_orchestrator
+namespace GatewayOrchestrator
 {
-    public static class GatewayOrchestrator
+    public class GatewayOrchestrator
     {
         [FunctionName("GatewayOrchestrator")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
         //[return: ServiceBus("d2c-messages", Connection = "gateway-orchestrator-sb-conn", EntityType = Microsoft.Azure.WebJobs.ServiceBus.EntityType.Topic)]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-             [ServiceBus("d2c-messages", Connection = "gateway-orchestrator-sb-conn", EntityType = EntityType.Topic)] ICollector<string> topicCollector,
+        public async Task<IActionResult> Run(
+             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+             [ServiceBus("d2c-messages", Connection = "gateway-orchestrator-sb-conn", EntityType = EntityType.Topic)] ICollector<dynamic> topicCollector,
             ILogger log)
         {
             log.LogInformation("GatewayOrchestrator: HTTP trigger function processed a request.");
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //get device id
+            string deviceId = req.Query["deviceId"];
+            if(string.IsNullOrEmpty(deviceId))
+                return (ActionResult)new BadRequestObjectResult("Invalid request parameters");
 
-            string d2cPayload = data?.d2cPayload;
-            if (string.IsNullOrEmpty(d2cPayload))
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            if (string.IsNullOrEmpty(requestBody))
                 return (ActionResult)new BadRequestObjectResult("Invalid request data");
             
-            topicCollector.Add(d2cPayload);
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //Here i'm assuming the payload doesn't include the deviceId, adding it here:
+            data.deviceId = deviceId;
+            
+            topicCollector.Add(data);
             return (ActionResult)new OkResult();
         }
     }
