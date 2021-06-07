724d22f44040f4b01cf4a2b4baffb7232c9d7bd3
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2021-06-07 20:15:52 +0400

    New Gateway Orchestrator implementation using dapr

---
+++ src/GatewayOrchestrator/Controllers/GatewayOrchestratorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayOrchestrator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GatewayOrchestratorController : ControllerBase
+    {
+        private ILogger<GatewayOrchestratorController> logger;
+
+        public GatewayOrchestratorController(ILogger<GatewayOrchestratorController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
---
+++ src/GatewayOrchestrator/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayOrchestrator
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers()
+                .AddDapr();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gateway Orchestrator", Version = "v1" });
+            });
+            string appInsightsConnection = Configuration["APPINSIGHTS_CONNECTIONSTRING"];
+            services.AddApplicationInsightsTelemetry(appInsightsConnection);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway Orchestrator v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCloudEvents();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapSubscribeHandler();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
