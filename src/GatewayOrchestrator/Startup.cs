4f8f5c0c35e06c70bd78773522802c8d3a6dac6a
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2021-08-23 10:58:19 +0400

    Various updates and fixes

--- src/GatewayOrchestrator/Startup.cs
+++ src/GatewayOrchestrator/Startup.cs
@@ -55,8 +55,6 @@ namespace GatewayOrchestrator
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gateway Orchestrator v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
