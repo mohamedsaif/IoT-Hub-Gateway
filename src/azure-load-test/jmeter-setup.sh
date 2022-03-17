7e5288a7c52f0f241b4d80d26572980ee5734d54
Mo <mohamed.saif@outlook.com>
2022-03-17 13:00:39 +0400

    adding azure-load-test

---
+++ src/azure-load-test/jmeter-setup.sh
@@ -0,0 +1,7 @@
+# Install Java OpenJDK (8+)
+java --version
+# Download JMeter https://jmeter.apache.org/download_jmeter.cgi
+# Open JMeter bin folder in terminal and run 'jmeter' or run jmeter.bat in file explorer
+# GUI Mode (good for editing the tests)
+# NOTE: If the UI is not working properley, just change the Options -> Look and feel -> Windows theme 
+
